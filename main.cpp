de69ba0c298ac8171bb92f0999a1b159de67a1cb
atl87 <abhilash.thekkilakattil@gmail.com>
2016-10-14 14:33:28 +0200

    Today's work: Fri 14 Oct 2016

--- main.cpp
+++ main.cpp
@@ -40,7 +40,7 @@ int main(int argc, char** argv) {
     
     int MAX_TIME=10000;
     float MIN_UTIL=1.0000;
-    float MAX_UTIL=NO_OF_PROCESSORS/2+0.01;
+    float MAX_UTIL=(NO_OF_PROCESSORS/2)+0.1000;
     
     varying_tasks(MAX_NO_OF_TASKS, number_of_tasks, MIN_UTIL, MAX_UTIL, MAX_TASKSETS_PER_SIMULATION, MIN_PERIOD, MAX_PERIOD, npr_percentage, NO_OF_PROCESSORS, MAX_TIME);
     varying_processors(MAX_NO_OF_TASKS, MIN_UTIL, MAX_UTIL, MAX_TASKSETS_PER_SIMULATION, MIN_PERIOD, MAX_PERIOD, npr_percentage, MIN_NO_PROC, MAX_NO_PROC, MAX_TIME);
