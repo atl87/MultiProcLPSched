085c18040ae1c29c3712aefb3df4df0cef4507ac
atl87 <abhilash.thekkilakattil@gmail.com>
2016-10-14 08:58:40 +0200

    First correctly running version

--- build/Debug/GNU-Linux/main.o.d
+++ build/Debug/GNU-Linux/main.o.d
@@ -1,5 +1,5 @@
 build/Debug/GNU-Linux/main.o: main.cpp Tasks.h Task_Gen.h \
- fps_schedulers.h edf_schedulers.h
+ fps_schedulers.h edf_schedulers.h fps_experiments.h
 
 Tasks.h:
 
@@ -8,3 +8,5 @@ Task_Gen.h:
 fps_schedulers.h:
 
 edf_schedulers.h:
+
+fps_experiments.h:
---
+++ fps_experiments.h
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   fps_experiments.h
+ * Author: abhilash
+ *
+ * Created on October 14, 2016, 8:32 AM
+ */
+
+#ifndef FPS_EXPERIMENTS_H
+#define FPS_EXPERIMENTS_H
+
+void varying_tasks(int MAX_NO_OF_TASKS, int number_of_tasks, int MAX_TASKSETS_PER_SIMULATION, int MIN_PERIOD, int MAX_PERIOD, float DEADLINE_FRACTION, float npr_percentage, float NO_OF_PROCESSORS, int MAX_TIME);
+
+
+#endif /* FPS_EXPERIMENTS_H */
+
