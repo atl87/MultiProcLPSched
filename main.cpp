dfd4daa763d31514036100ccb81438cec130f51d
atl87 <abhilash.thekkilakattil@gmail.com>
2017-02-13 09:42:05 +0100

    First commit to this branch

--- main.cpp
+++ main.cpp
@@ -23,8 +23,8 @@ using namespace std;
  */
 int main(int argc, char** argv) {
     
-    int MAX_NO_OF_TASKS=32;
-    int MAX_TASKSETS_PER_SIMULATION=1000;
+    int MAX_NO_OF_TASKS=36;
+    int MAX_TASKSETS_PER_SIMULATION=500;
     int number_of_tasks=6;
     int MAX_PERIOD=490;
     int MIN_PERIOD=10;
